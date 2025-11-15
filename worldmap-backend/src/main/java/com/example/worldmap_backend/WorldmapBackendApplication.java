a560e90e32f81b818b73ffe4fffea99b3c2159ba
Kevin Llanos <kllano2@wgu.edu>
2025-11-15 16:50:53 -0600

    Add worldmap-backend: initial import

---
+++ worldmap-backend/src/main/java/com/example/worldmap_backend/WorldmapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.worldmap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldmapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldmapBackendApplication.class, args);
+	}
+
+}
