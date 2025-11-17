6e27aef2130f9f29e4ca0f2a27748cc71a801d95
Val Lysenko <kakao757@gmail.com>
2025-11-16 20:42:49 -0800

    setup NextAuth and Prisma

---
+++ next-app/lib/auth.ts
@@ -0,0 +1,31 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { type DefaultSession, type NextAuthConfig } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { db } from './db';
+
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession['user'];
+  }
+}
+
+export const authConfig = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(db),
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
+} satisfies NextAuthConfig;
