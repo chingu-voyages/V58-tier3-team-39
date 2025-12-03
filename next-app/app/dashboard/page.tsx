0b96372b0d8bf53221b461e9a2fb866b915f3d71
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-12-03 11:44:42 -0800

    add map, charts and stacard

---
+++ next-app/app/dashboard/page.tsx
@@ -0,0 +1,85 @@
+'use client';
+import { useState } from 'react';
+import { getDemographipcsStats, members } from '@/lib/dataHelpers';
+import { processMemberData } from '@/lib/memberStats';
+import chinguData from '@/app/data/chingu_info.json';
+
+import DonutChartComponent from '@/components/DonutChart';
+import PieChartComponent from '@/components/PieChart';
+import StatCard from '@/components/StatCard';
+import { Briefcase, MapPin, User } from 'lucide-react';
+import Map from '@/components/Map';
+
+const Dashboard = () => {
+  const stats = getDemographipcsStats(members);
+
+  const countryStats = processMemberData(chinguData as any);
+
+  const [memberCount, setMemberCount] = useState(0);
+  const [countryCount, setCountryCount] = useState(0);
+
+  const handleMemberCountChange = (count: number, countries: number) => {
+    setMemberCount(count);
+    setCountryCount(countries);
+  };
+
+  return (
+    <main
+      className="
+  w-full min-h-screen px-4 md:px-8 pt-20 md:py-30 
+  overflow-x-auto bg-[#F1F5F9]
+
+  md:grid 
+  md:grid-cols-[1fr_2fr]  
+  md:grid-rows-2 auto
+  md:gap-6
+"
+    >
+      {/* LEFT COL — STAT CARDS */}
+      <div className="flex flex-col gap-4 md:gap-10 md:row-span-2 mb-4">
+        <StatCard
+          title="Common Role"
+          icon={<Briefcase />}
+          item={stats.commonRole?.name ?? 'N/A'}
+          value={stats.commonRole?.value ?? 0}
+        />
+        <StatCard
+          title="Common Location"
+          icon={<MapPin />}
+          item={stats.commonLocation?.name ?? 'N/A'}
+          value={stats.commonLocation?.value ?? 0}
+        />
+        <StatCard
+          title="Common Tier"
+          icon={<User />}
+          item={stats.commonTier?.name ?? 'N/A'}
+          value={stats.commonTier?.value ?? 0}
+        />
+      </div>
+
+      {/* RIGHT COL — Map */}
+      <div className="w-full h-[400px] md:h-full">
+        <Map
+          countryStats={countryStats}
+          onMemberCountChange={handleMemberCountChange}
+        />
+      </div>
+
+      {/* RIGHT COL — Charts */}
+      <div className="flex flex-col md:flex-row gap-4 w-full min-h-[300px]">
+        <div className="flex-1 flex">
+          <PieChartComponent title="Voyage Roles" data={stats.roleChart} />
+        </div>
+
+        <div className="flex-1 flex">
+          <DonutChartComponent
+            title="Gender Breakdown"
+            data={stats.genderChart}
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Dashboard;
