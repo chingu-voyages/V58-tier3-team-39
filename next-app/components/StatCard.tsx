ff3e6e25b7bdea79171d07cea4247d9b212c1174
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-12-03 11:39:07 -0800

    update css and props

--- next-app/components/StatCard.tsx
+++ next-app/components/StatCard.tsx
@@ -1,22 +1,26 @@
 type StatCardProps = {
   title: string;
   icon: React.ReactNode;
-  item: string | number;
-  number?: string | number;
+  item?: string;
+  value?: number;
 };
 
-const StatCard = ({ title, icon, item, number }: StatCardProps) => {
+const StatCard = ({ title, icon, item, value }: StatCardProps) => {
   return (
-    <div className="bg-white flex h-36 py-3 px-4 flex-col rounded-2xl">
+    <div className="bg-white flex h-36 py-3 px-4 md:py-5 md:px-10 flex-col rounded-2xl">
       <div className="flex justify-between">
-        <h1 className="text-2xl font-semibold text-gray-500">{title}</h1>
-        <div className="text-2xl text-gray-500">{icon}</div>
+        <h1 className="text-lg md:text-2xl font-semibold text-gray-500">
+          {title}
+        </h1>
+        <div className="text-lg md:text-2xl text-gray-500">{icon}</div>
       </div>
 
-      <div className="flex justify-between">
-        <div className="text-3xl font-semibold">{item}</div>
-        {number !== undefined && (
-          <div className="text-3xl font-semibold text-blue-brand">{number}</div>
+      <div className="grow flex items-center justify-between">
+        <div className="text-lg md:text-3xl font-semibold">{item}</div>
+        {value !== undefined && (
+          <div className="text-lg md:text-3xl font-semibold text-blue-brand">
+            {value}
+          </div>
         )}
       </div>
     </div>
