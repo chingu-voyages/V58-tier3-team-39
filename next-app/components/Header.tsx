0f0e63b3dff7fd254651e309b831c3a640336939
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2025-12-13 16:27:01 +0100

    fix mobile nav dark mode

--- next-app/components/Header.tsx
+++ next-app/components/Header.tsx
@@ -6,10 +6,9 @@ import { Menu, Moon, X } from 'lucide-react';
 import { signOut } from 'next-auth/react';
 import { ThemeToggle } from './ThemeToggle';
 import Chingu from './icons/Chingu';
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
 import Button from './ui/Button';
 import Google from './icons/Google';
-
 
 interface HeaderProps {
   session: {
@@ -21,21 +20,22 @@ interface HeaderProps {
 
 const Header = ({ session }: HeaderProps) => {
   const [isActive, setIsActive] = useState(false);
-  const pathname = usePathname()
+  const pathname = usePathname();
 
-
-const linkTextStyle = (href: string) => 
-  pathname === href ?
-    'text-blue-brand' : 
-    'text-secondary-text hover:text-blue-brand/50'
+  const linkTextStyle = (href: string) =>
+    pathname === href
+      ? 'text-blue-brand'
+      : 'text-secondary-text hover:text-blue-brand/50';
 
   return (
     <>
       <header className="flex mx-auto h-14 md:h-20 w-full bg-background items-center fixed top-0 z-20 font-semibold px-4 md:px-8 gap-10">
         <Link href="/" className="font-bold mr-6">
-          <div className='flex gap-2 items-center'>
-            <Chingu className='h-11 w-11'/>
-            <div className={`flex flex-col leading-5 tracking-widest text-sm md:text-base lg:text-lg ${pathname !== '/' ? 'hover:text-blue-brand/50' : ''}`}>
+          <div className="flex gap-2 items-center">
+            <Chingu className="h-11 w-11" />
+            <div
+              className={`flex flex-col leading-5 tracking-widest text-sm md:text-base lg:text-lg ${pathname !== '/' ? 'hover:text-blue-brand/50' : ''}`}
+            >
               <span>CHINGU</span>
               <span>DEMOGRAPHICS</span>
             </div>
@@ -43,16 +43,26 @@ const linkTextStyle = (href: string) =>
         </Link>
 
         <nav className="hidden md:flex items-center gap-6 flex-1">
-          <Link href="/map" className={linkTextStyle('/map')}>Map</Link>
-          <Link href="/list" className={linkTextStyle('/list')}>List</Link>
-          <Link href="/dashboard" className={linkTextStyle('/dashboard')}>Dashboard</Link>
-          
+          <Link href="/map" className={linkTextStyle('/map')}>
+            Map
+          </Link>
+          <Link href="/list" className={linkTextStyle('/list')}>
+            List
+          </Link>
+          <Link href="/dashboard" className={linkTextStyle('/dashboard')}>
+            Dashboard
+          </Link>
+
           <div className="ml-auto flex items-center gap-6">
             <ThemeToggle />
             {session?.user?.name && <span>Welcome {session.user.name}</span>}
             {session ? (
-              <Button variant="secondary" className="flex gap-1 px-4! py-2! text-sm!" onClick={() => signOut()}>
-                <Google/> Sign Out
+              <Button
+                variant="secondary"
+                className="flex gap-1 px-4! py-2! text-sm!"
+                onClick={() => signOut()}
+              >
+                <Google /> Sign Out
               </Button>
             ) : (
               <Link href="/api/auth/signin" className="cursor-pointer">
@@ -80,7 +90,7 @@ const linkTextStyle = (href: string) =>
           />
 
           <nav
-            className={`fixed top-0 right-0 h-full w-2/3 bg-white text-[#636363] z-30 shadow-xl p-6 pt-14 flex flex-col gap-8 md:hidden transform transition-transform duration-300 ${isActive ? 'translate-x-0' : 'translate-x-full'}`}
+            className={`fixed top-0 right-0 h-full w-2/3 bg-secondary-bg z-30 shadow-xl p-6 pt-14 flex flex-col gap-8 md:hidden transform transition-transform duration-300 ${isActive ? 'translate-x-0' : 'translate-x-full'}`}
           >
             <button
               onClick={() => setIsActive(false)}
